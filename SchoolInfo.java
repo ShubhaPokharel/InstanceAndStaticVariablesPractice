883207eeea03eba4a82208d7c9d9502235ab87e9
˗ˏˋShubha ´ˎ˗ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-06-20 21:01:45 -0400

    Add files via upload

---
+++ SchoolInfo.java
@@ -0,0 +1,26 @@
+package com.wipro.variables;
+
+public class SchoolInfo {
+	int stuId = 3702;
+    String name = "Shubha";
+	static int stuMarks = 100;
+	static String schoolName = "Harrison Education";
+	static int schoolId = 9308;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolInfo info = new SchoolInfo();
+		String s = info.name;
+		System.out.println("Name of the student: "+ s);
+		
+		int id = info.stuId;
+		System.out.println("Student Id: "+id);
+		
+		System.out.println("Student Marks: " + SchoolInfo.stuMarks);
+		System.out.println("School Name: " + SchoolInfo.schoolName);
+		System.out.println("School Info: " + SchoolInfo.schoolId);
+		
+
+	}
+
+}
